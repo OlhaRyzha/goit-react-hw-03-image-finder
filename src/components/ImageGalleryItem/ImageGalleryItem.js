4c62224ec96144401035e901ccc02a137fddca1b
OlgaRyzha <orizha11@gmail.com>
2023-02-16 13:53:25 +0200

    +

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -5,36 +5,13 @@ export class ImageGalleryItem extends Component {
   state = {
     showModal: false,
   };
-  onImgClick = e => {
-    this.setState({
-      showModal: !false,
-    });
-  };
-  onImgKeyDown = e => {
-    if (this.state.isBtnClick === false) return;
-    if (e.key === 'Escape') {
-      this.setState({
-        showModal: false,
-      });
-    }
-  };
-  onOverlayClick = e => {
-    if (this.state.isBtnClick === false) return;
-    if (e.target.classList.contains('overlay')) {
-      this.setState({
-        showModal: false,
-      });
-    }
+
+  onImgClick = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
   };
 
-  componentWillUnmount() {
-    window.removeEventListener('click', this.onOverlayClick);
-    window.removeEventListener('keydown', this.onImgKeyDown);
-  }
-  componentDidMount() {
-    window.addEventListener('click', this.onOverlayClick);
-    window.addEventListener('keydown', this.onImgKeyDown);
-  }
   render() {
     const { webformatURL, largeImageURL } = this.props.img;
 
@@ -42,7 +19,7 @@ export class ImageGalleryItem extends Component {
       <GalleryItem>
         <ImageGallery onClick={this.onImgClick} src={webformatURL} />
         {this.state.showModal && (
-          <Modal>
+          <Modal onClose={this.onImgClick}>
             <img src={largeImageURL} alt="" />
           </Modal>
         )}
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,15 +1,38 @@
 import { Overlay, ModalEl } from './Modal.styled';
+import { Component } from 'react';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export function Modal({ children }) {
-  return createPortal(
-    <>
-      <Overlay className="overlay">
-        <ModalEl>{children}</ModalEl>
-      </Overlay>
-    </>,
-    modalRoot
-  );
+export class Modal extends Component {
+  onImgKeyDown = e => {
+    if (e.key === 'Escape') {
+      this.props.onClose();
+    }
+  };
+  onOverlayClick = e => {
+    if (e.target.classList.contains('overlay')) {
+      this.props.onClose();
+    }
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onOverlayClick);
+    window.removeEventListener('keydown', this.onImgKeyDown);
+  }
+  componentDidMount() {
+    window.addEventListener('click', this.onOverlayClick);
+    window.addEventListener('keydown', this.onImgKeyDown);
+  }
+  render() {
+    const { children } = this.props;
+    return createPortal(
+      <>
+        <Overlay className="overlay">
+          <ModalEl>{children}</ModalEl>
+        </Overlay>
+      </>,
+      modalRoot
+    );
+  }
 }
